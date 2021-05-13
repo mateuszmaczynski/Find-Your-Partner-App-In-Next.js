6e4aa7b792b515731b35da22913605c00c7afe47
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-05-13 22:25:21 +0200

    add examples of create/update/delete data using prisma in api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,41 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { user } from "models";
 
-export default (req, res) => {
+export default async (req, res) => {
+
+  // const allUsers = await user.findMany({
+  //   select: {
+  //     id: true,
+  //     createdAt: true,
+  //     name: true,
+  //     email: true
+  //   },
+  // });
+
+  await user.create({
+    data: {
+      email: 'jannowak@wp.pl',
+      name: 'Jan Nowak',
+      image: 'test.jpg'
+    }
+  });
+
+  // await user.update({
+  //   where: {
+  //     id: 2
+  //   },
+  //   data: {
+  //     name: 'Piotr'
+  //   }
+  // });
+
+  // await user.delete({
+  //   where: {
+  //     id: 2
+  //   }
+  // });
+
+  const allUsers = await user.findMany();
+
   res.statusCode = 200;
-  res.json({ name: "John Doe" });
+  res.json(allUsers);
 };
