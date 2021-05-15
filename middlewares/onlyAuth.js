5ba92802200610eafd05efec7e7d937ab90d7c95
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-05-15 21:44:38 +0200

    add middleware onlyAuth

---
+++ middlewares/onlyAuth.js
@@ -0,0 +1,25 @@
+import { getSession } from 'next-auth/client';
+import { user } from 'models';
+
+const onlyAuth = (handler) => {
+  return async (req, res) => {
+    const session = await getSession({ req });
+
+    if (!session) {
+      return res.status(401).json({
+        success: false,
+        message: 'Please log in to get access.'
+      });
+    }
+
+    req.currentUser = await user.findUnique({
+      where: {
+        email: session.user.email
+      }
+    });
+
+    return handler(req, res);
+  };
+};
+
+export default onlyAuth;
