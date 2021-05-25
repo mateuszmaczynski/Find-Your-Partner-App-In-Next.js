51a56cce4b2b0946e0beb1f94d07d78b2d230bb1
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-05-25 21:35:30 +0200

    edit config models

--- models/index.js
+++ models/index.js
@@ -1,14 +1,3 @@
-import { PrismaClient } from "@prisma/client";
+import client from './client';
 
-let prisma;
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
-
-export const { conversation, user, filter, profileCheck } = prisma;
+export const { conversation, user, filter, profileCheck, skill, timezone } = client;
