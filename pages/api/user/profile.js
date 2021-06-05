fc24c50252948997b7bd477f70e92a48b5259b09
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-06-05 21:29:40 +0200

    modify endpoint userProfile

--- pages/api/user/profile.js
+++ pages/api/user/profile.js
@@ -1,8 +1,22 @@
 import onlyAuth from 'middlewares/onlyAuth';
+import { user as userModel } from 'models';
 import { updateProfile } from 'services/profiles/update';
 
 const userProfileApi = async (req, res) => {
   switch (req.method) {
+    case 'GET': {
+      const user = await userModel.findUnique({
+        where: {
+          id: req.currentUser.id
+        },
+        include: {
+          filter: true
+        }
+      });
+
+      res.status(200).json({ user });
+      break;
+    }
     case 'PUT': {
       try {
         const payload = req.body;
@@ -10,7 +24,6 @@ const userProfileApi = async (req, res) => {
 
         res.status(200).json({ user });
       } catch (error) {
-        console.log(`error`, error)
         res.status(422).json({ user: null, error });
       }
       break;
