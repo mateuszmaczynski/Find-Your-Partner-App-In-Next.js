a0061feb434a85af02997ac3ba10ef9acd76e4cd
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-06-07 17:51:48 +0200

    add components: Message, MyMessage

---
+++ components/Message/index.js
@@ -0,0 +1,21 @@
+export const Message = ({ message }) => {
+  return (
+    <div className="flex justify-start mb-4">
+      <img src={message.user.image} className="object-cover h-8 w-8 rounded-full" alt="" />
+      <div className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
+        {message.content}
+      </div>
+    </div>
+  );
+};
+
+export const MyMessage = ({ message }) => {
+  return (
+    <div className="flex justify-end mb-4">
+      <div className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
+        {message.content}
+      </div>
+      <img src={message.user.image} className="object-cover h-8 w-8 rounded-full" alt="" />
+    </div>
+  );
+};
